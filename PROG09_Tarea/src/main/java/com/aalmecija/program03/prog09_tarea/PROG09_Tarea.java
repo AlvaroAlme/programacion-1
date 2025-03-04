d8f7df3e4f76bcce31cca25ee77eb1fc6c05152f
AlvaroAlme <3888170@alu.murciaeduca.es>
2025-03-04 20:51:09 +0100

    Add files via upload

---
+++ PROG09_Tarea/src/main/java/com/aalmecija/program03/prog09_tarea/PROG09_Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aalmecija.program03.prog09_tarea;
+
+/**
+ *
+ * @author aalme
+ */
+public class PROG09_Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
