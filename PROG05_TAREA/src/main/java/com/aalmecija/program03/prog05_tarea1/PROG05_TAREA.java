1ae8d0f4b19eb07706d426e41321c478d09f9978
AlvaroAlme <3888170@alu.murciaeduca.es>
2025-03-04 20:51:38 +0100

    Add files via upload

---
+++ PROG05_TAREA/src/main/java/com/aalmecija/program03/prog05_tarea1/PROG05_TAREA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aalmecija.program03.prog05_tarea1;
+
+/**
+ *
+ * @author aalme
+ */
+public class PROG05_TAREA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
